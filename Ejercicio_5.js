4c8214a5b10572e5c11de219210844c81e95f902
Alvaro Corpeño <37373620+aacm13@users.noreply.github.com>
2018-09-28 09:38:00 -0600

    Update Ejercicio_5.js

--- Ejercicio_5.js
+++ Ejercicio_5.js
@@ -1,10 +1,10 @@
 var arr = [2, false, 1, 3, "hola", 5, "neta", 1];
 var type = 'number';
 function getTypoDato(array, type) {
-    for(let i =0;i<array.lenght;i++){
-        if(typeof array[i] == type){
-            console.log(array[i]);
+    array.forEach(function(v){
+        if(typeof v === type){
+        console.log(v)
         }
-    }
+    });
 }
 console.log(getTypoDato(arr, type));
