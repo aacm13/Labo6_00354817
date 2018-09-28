44c48bcf742a822783fb2de23ea27848475ce0a6
Alvaro Corpeño <37373620+aacm13@users.noreply.github.com>
2018-09-28 09:38:28 -0600

    Update Ejercicio_6.js

--- Ejercicio_6.js
+++ Ejercicio_6.js
@@ -1,6 +1,36 @@
 function incriptar(palabra){
-    for(let i =0;i<palabra.length;i++){
-        palabra.replace("m","0");
+    for(var i=0; i<palabra.length;i++){
+        if(palabra[i] === 'm'){
+            palabra[i] = 0;
+        }
+        if(palabra[i] === 'u'){
+            palabra[i] = 1;
+        }
+        if(palabra[i] === 'r'){
+            palabra[i] = 2;
+        }
+        if(palabra[i] === 'c'){
+            palabra[i] = 3;
+        }
+        if(palabra[i] === 'i'){
+            palabra[i] = 4;
+        }
+        if(palabra[i] === 'e'){
+            palabra[i] = 5;
+        }
+        if(palabra[i] === 'l'){
+            palabra[i] = 6;
+        }
+        if(palabra[i] === 'a'){
+            palabra[i] = 7;
+        }
+        if(palabra[i] === 'g'){
+            palabra[i] = 8;
+        }
+        if(palabra[i] === 'o'){
+            palabra[i] = 9;
+        }
+        
     }
     return palabra;
 }
